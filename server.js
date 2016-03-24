5daef4a26f313d616b57a8d519f19402249e5164
Trewaters <trewaters@hotmail.com>
2016-03-24 05:16:51 -0700

    [UPDATE] small test changes to the server.js. added some ReadMe text.
    quick test again bart api module

--- server.js
+++ server.js
@@ -1,34 +1,56 @@
 var express = require('express');
 var app = express();
 
-//[reference] https://www.npmjs.com/package/morgan
-var logger = require('morgan');
+var config = require('./config_bartapi.js');
+
+var bart = require('bay-area-rapid-transit');
+
 //[reference] https://www.npmjs.com/package/body-parser
 var bodyParser = require('body-parser');
 
-// [TO DO] - remove until I have a config file to point to
-//var config = require('./config_bartapi.js');
+//[reference] https://www.npmjs.com/package/morgan
+var logger = require('morgan');
+app.use(morgan('dev')); // dev format (:method :url :status :response-time ms - :res[content-length])
 
-var index = require('./routes/index');
+
+//app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','jade');
 
-app.use('/',index);
+/*
+var index = require('./routes/index');
 
-app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use('/',index);
+*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(morgan('dev')); // dev format (:method :url :status :response-time ms - :res[content-length])
-
-app.use(express.static(__dirname + 'public'));
+//app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 
 app.get('/',function(req, res){
-    res.send('BART api website is running.');
+//    bart.advisories(params);
+    
+    res.send('BART api website is running. params = ' + JSON.stringify(bart.advisories(params)));
 });
 
-app.listen(3333, function(){
-    console.log("app.listen on port 3333");
+app.post('/post_this',function(req,res){
+    res.send('BART api "post_this"');
+});
+
+app.delete('/delete_this',function(req,res){
+    res.send('BART api "delete_this"');
+});
+
+app.put('/put_this',function(req,res){
+    res.send('BART api "put_this"');
+});
+
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log("app.listen on port 3000, http://%s%s", host, port);
 });
