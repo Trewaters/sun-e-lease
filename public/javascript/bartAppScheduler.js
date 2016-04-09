53da305e73b59410fc34c44fee9f8b8575e0b1f3
Trewaters <trewaters@hotmail.com>
2016-04-09 10:39:17 -0700

    [ADD] Get destination station long and lat in Angular. Renamed route for listing all BART stations

--- public/javascript/bartAppScheduler.js
+++ public/javascript/bartAppScheduler.js
@@ -19,56 +19,91 @@ app.factory('youAreHere', function($resource, $q, $rootScope) {
     return $resource('/yah/here', {});
 });
 
+app.factory('listStations', function($resource, $q, $rootScope) {
+    return $resource('/yah/listAllStations', {});
+});
+
 //---
 // controllers
 //---
-app.controller('mainScreen', function($scope, youAreHere) {
-    
+app.controller('mainScreen', function($scope, youAreHere, listStations) {
+
     $scope.message = "Main Screen controller";
+
+    $scope.vLatYAH = "0";
+    $scope.vLatYAH = "0";
     
-     $scope.vLat = "0";
-     $scope.vLong = "0";
-     $scope.accuracy = "0";
-     $scope.error = "";
-     
-     $scope.showResult = function(){
-         return $scope.error == "";
-     };
-     
-    $scope.aShowPosition = function(position){
-        $scope.vLat = position.coords.latitude;
-        $scope.vLong = position.coords.longitude;
+    $scope.vLongDS = "0";
+    $scope.vLongDS = "0";
+    
+    $scope.vAcc = "0";
+    $scope.error = "";
+    $scope.selectedStationYAH = "";
+    $scope.selectedStationDS = "";
+
+    $scope.aShowPosition = function(position) {
+        $scope.vLatDS = position.coords.latitude;
+        $scope.vLongDS = position.coords.longitude;
         $scope.vAcc = position.coords.accuracy;
         $scope.$apply();
     };
-    
-    $scope.aLocation = function(){
-        if(navigator.geolocation){
+
+    $scope.aLocation = function() {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition($scope.aShowPosition)
-        }else{
+        } else {
             $scope.error = "Angular Geolocation is not supported by this browser.";
         };
     };
-     
-     $scope.showError = function (error) {
-            switch (error.code) {
-                case error.PERMISSION_DENIED:
-                    $scope.error = "User denied the request for Geolocation."
-                    break;
-                case error.POSITION_UNAVAILABLE:
-                    $scope.error = "Location information is unavailable."
-                    break;
-                case error.TIMEOUT:
-                    $scope.error = "The request to get user location timed out."
-                    break;
-                case error.UNKNOWN_ERROR:
-                    $scope.error = "An unknown error occurred."
-                    break;
-            }
-            $scope.$apply();
+
+    $scope.showResult = function() {
+        return $scope.error == "";
+    };
+    
+    $scope.showError = function(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                $scope.error = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                $scope.error = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                $scope.error = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                $scope.error = "An unknown error occurred."
+                break;
         }
-     $scope.aLocation();
-     
-     $scope.stations = youAreHere.query();
-     
+        $scope.$apply();
+    }
+
+    $scope.aLocation();
+
+    // list all stations as an array
+    // key values are all arrays also
+    // keys = name, abbr, gtfs_latitude, gtfs_longitude, address, city, county, state, zipcode
+    $scope.stations = listStations.query();
+
+$scope.hereMap = function(){
+        // current location abbreviation = $scope.selectedStationYAH
+        for(var i=0; i < $scope.stations.length; i++ ){
+            
+            var vStaObj = JSON.parse(JSON.stringify($scope.stations[i]));
+            
+            var strStaObj = JSON.stringify(vStaObj.abbr);
+            var strSelectedStationYAH = JSON.stringify($scope.selectedStationYAH);
+            
+            if (strStaObj === strSelectedStationYAH){
+                            
+              $scope.vStaDetailsYAH = JSON.stringify($scope.stations[i]);
+              $scope.vLatYAH = vStaObj.gtfs_latitude[0];
+              $scope.vLongYAH = vStaObj.gtfs_longitude[0];
+              
+              return $scope.stations[i];
+                
+            };
+        };
+    };
+    
 });
--- routes/yah.js
+++ routes/yah.js
@@ -16,7 +16,20 @@ var vShow = '';
 var str = '';
 var vParsed = '';
 
+/*
 router.route('/here')
+    .get(function(req, res) {
+        
+        var vAbbr = req.params.abbr;
+        
+        var latlon = position.coords.latitude + "," + position.coords.longitude;
+        var img_url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false";
+            
+            "<br><img src='" + img_url + "'>"
+           
+        });
+     */
+router.route('/listAllStations')
     .get(function(req, res) {
         vCmd = 'stns';
 
@@ -26,29 +39,44 @@ router.route('/here')
         };
 
         callback = function(response) {
-            
+
             response.on('data', function(chunk) {
                 vParsed += chunk;
                 //console.log('vParsed = ' + vParsed);
             });
-            
+
             response.on('end', function() {
                 parseString(vParsed, function(err, result) {
                     vShow = JSON.stringify(result);
-                    
+
                     //console.log('stations = ' + vShow);
                     console.log("result.root.uri = " + JSON.stringify(result.root.uri) + "\n");
-                    console.log("result.root.stations = " + util.inspect(result.root.stations,{showHidden:false, depth:null}) + "\n");
+                    console.log("result.root.stations = " + util.inspect(result.root.stations, { showHidden: false, depth: null }) + "\n");
                     console.log("result.root.stations.station.name = " + JSON.stringify(result.root.stations[0].station[0].name) + "\n");
-                    
-                    return res.jsonp(result.root.stations);
+
+                    console.log("stations array length = " + result.root.stations.length);
+                    var vIndexNum = result.root.stations.length - 1;
+                    console.log("list station number = " + result.root.stations[vIndexNum].station.length);
+                    var vStaNum = result.root.stations[vIndexNum].station.length;
+
+                    // fun random numbers for data selection
+                    function randomIntFromInterval(min, max) {
+                        return Math.floor(Math.random() * (max - min + 1) + min);
+                    };
+
+                    var vRando = randomIntFromInterval(1, vStaNum);
+
+                    //console.log(result.root.stations[vIndexNum].station[vRando].name + " station coords = \nLatitude (" + result.root.stations[vIndexNum].station[vRando].gtfs_latitude + "), \nLongitude (" + result.root.stations[vIndexNum].station[vRando].gtfs_longitude + ")\n");
+
+                    return res.send(result.root.stations[0].station);
                 });
             });
-            
+
         };
 
         http.request(options, callback).end();
-        
+
+
     });
 
 module.exports = router;
