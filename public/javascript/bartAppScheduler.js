5b7fbf457802d31bf9c4236e86d5246ad7ad9b21
Trewaters <trewaters@hotmail.com>
2016-04-17 08:12:28 -0700

    [ADD] added filter for when fields in returned data are empty so it won't break the application.
    [ADD] added a function to get stnsched from API. Station schedule shows all the trains at a specific station for the day.

--- public/javascript/bartAppScheduler.js
+++ public/javascript/bartAppScheduler.js
@@ -1,5 +1,5 @@
 var app = angular.module("bartAppScheduler", ['ngRoute', 'ngResource'])
-    .config(function($routeProvider) {
+    .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'main.html',
@@ -20,60 +20,64 @@ app.factory('youAreHere', function($resource, $q, $rootScope) {
 });
 */
 
-app.factory('listStations', function($resource, $q, $rootScope) {
+app.factory('listStations', function ($resource, $q, $rootScope) {
     return $resource('/yah/listAllStations', {});
 });
 
-app.factory('departTime',function($resource,$q,$rootScope){
-   return $resource('/yah/departTimeStation',{}); 
-   
-   // required parameter 'vOriginStation' as string
+app.factory('departTime', function ($resource, $q, $rootScope) {
+    return $resource('/yah/departTimeStation', {});
+    // required parameter 'vOriginStation' as string
+});
+
+app.factory('stationSchedule', function ($resource, $q, $rootScope) {
+    return $resource('/yah/stationSched', {});
+    // required parameter 'vOriginStation' as string
 });
 
 //---
 // controllers
 //---
-app.controller('mainScreen', function($scope, listStations, departTime) {
+app.controller('mainScreen', function ($scope, listStations, departTime, stationSchedule) {
 
     $scope.selectedStationYAH = "";
     $scope.vLatYAH = "0";
     $scope.vLongYAH = "0";
     //$scope.vAccYAH = "0";
-        
+
     $scope.selectedStationDS = "";
     $scope.vLatDS = "0";
     $scope.vLongDS = "0";
     //$scope.vAccDS = "0";
-    
+
     $scope.error = "";
-    
-/*
-    // [NOTE] 4/9/2016 - not using this at the moment
-    $scope.aShowPosition = function(position) {
-        $scope.vLatYAH = position.coords.latitude;
-        $scope.vLongYAH = position.coords.longitude;
-        //$scope.vAccYAH = position.coords.accuracy;
-        $scope.$apply();
-    };
 
-    // [NOTE] 4/9/2016 - not using this at the moment
-    $scope.aLocation = function() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition($scope.aShowPosition)
-        } else {
-            $scope.error = "Angular Geolocation is not supported by this browser.";
+    /*
+        // [NOTE] 4/9/2016 - not using this at the moment
+        $scope.aShowPosition = function(position) {
+            $scope.vLatYAH = position.coords.latitude;
+            $scope.vLongYAH = position.coords.longitude;
+            //$scope.vAccYAH = position.coords.accuracy;
+            $scope.$apply();
         };
-    };
     
-    // [NOTE] 4/9/2016 - not using this at the moment
-    //$scope.aLocation();
-*/
+        // [NOTE] 4/9/2016 - not using this at the moment
+        $scope.aLocation = function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition($scope.aShowPosition)
+            } else {
+                $scope.error = "Angular Geolocation is not supported by this browser.";
+            };
+        };
+        
+        // [NOTE] 4/9/2016 - not using this at the moment
+        //$scope.aLocation();
+    */
 
-    $scope.showResult = function() {
+    $scope.showResult = function () {
         return $scope.error == "";
     };
-    
-    $scope.showError = function(error) {
+
+    $scope.showError = function (error) {
         switch (error.code) {
             case error.PERMISSION_DENIED:
                 $scope.error = "User denied the request for Geolocation."
@@ -90,71 +94,80 @@ app.controller('mainScreen', function($scope, listStations, departTime) {
         }
         $scope.$apply();
     }
-    
+
     // list all stations as an array
     // key values are all arrays also
     // keys = [ name, abbr, gtfs_latitude, gtfs_longitude, address, city, county, state, zipcode ]
     $scope.stations = listStations.query();
 
-$scope.hereMapYAH = function(){
+    $scope.hereMapYAH = function () {
         // current location abbreviation = $scope.selectedStationYAH
-        for(var i=0; i < $scope.stations.length; i++ ){
-            
+        for (var i = 0; i < $scope.stations.length; i++) {
+
             var vStaObj = JSON.parse(JSON.stringify($scope.stations[i]));
-            
+
             var strStaObj = JSON.stringify(vStaObj.abbr);
             var strSelectedStationYAH = JSON.stringify($scope.selectedStationYAH);
-            
-            if (strStaObj === strSelectedStationYAH){
-                            
-              $scope.vStaDetailsYAH = JSON.stringify($scope.stations[i]);
-              $scope.vLatYAH = vStaObj.gtfs_latitude[0];
-              $scope.vLongYAH = vStaObj.gtfs_longitude[0];
-              
-              //return $scope.stations[i];
-              return $scope.vStaDetailsYAH;
-                
+
+            if (strStaObj === strSelectedStationYAH) {
+
+                $scope.vStaDetailsYAH = JSON.stringify($scope.stations[i]);
+                $scope.vLatYAH = vStaObj.gtfs_latitude[0];
+                $scope.vLongYAH = vStaObj.gtfs_longitude[0];
+
+                //return $scope.stations[i];
+                return $scope.vStaDetailsYAH;
+
             };
         };
     };
-    
-    $scope.hereMapDS = function(){
+
+    $scope.hereMapDS = function () {
         // current location abbreviation = $scope.selectedStationDS
-        for(var i=0; i < $scope.stations.length; i++ ){
-            
+        for (var i = 0; i < $scope.stations.length; i++) {
+
             var vStaObj = JSON.parse(JSON.stringify($scope.stations[i]));
-            
+
             var strStaObj = JSON.stringify(vStaObj.abbr);
             var strSelectedStationDS = JSON.stringify($scope.selectedStationDS);
-            
-            if (strStaObj === strSelectedStationDS){
-                            
-              $scope.vStaDetailsDS = JSON.stringify($scope.stations[i]);
-              $scope.vLatDS = vStaObj.gtfs_latitude[0];
-              $scope.vLongDS = vStaObj.gtfs_longitude[0];
-              
-              //return $scope.stations[i];
-              return $scope.vStaDetailsDS;
-                
+
+            if (strStaObj === strSelectedStationDS) {
+
+                $scope.vStaDetailsDS = JSON.stringify($scope.stations[i]);
+                $scope.vLatDS = vStaObj.gtfs_latitude[0];
+                $scope.vLongDS = vStaObj.gtfs_longitude[0];
+
+                //return $scope.stations[i];
+                return $scope.vStaDetailsDS;
+
             };
         };
     };
 
-//$scope.nextTrain = departTime.get({'vOriginStation':'19TH'});
-//$scope.nextTrain = departTime.query({'vOriginStation':$scope.selectedStationYAH})
-$scope.nextTrain = function(){
-    
-    
-    if($scope.selectedStationYAH == '' || $scope.selectedStationYAH == null){
-        //$scope.showNextTrainTime = departTime.get({'vOriginStation':'19TH'});
-        $scope.showNextTrainTime = departTime.query({'vOriginStation':'19TH'});
-    }else{
-        //$scope.showNextTrainTime = departTime.get({'vOriginStation': $scope.selectedStationYAH});
-        $scope.showNextTrainTime = departTime.query({'vOriginStation': $scope.selectedStationYAH});
-    };
-    
-    return $scope.showNextTrainTime;
-}
+    $scope.nextTrain = function () {
 
+        if ($scope.selectedStationYAH == '' || $scope.selectedStationYAH == null) {
+            //$scope.showNextTrainTime = departTime.get({'vOriginStation':'19TH'});
+            $scope.showNextTrainTime = departTime.query({ 'vOriginStation': '19TH' });
+        } else {
+            //$scope.showNextTrainTime = departTime.get({'vOriginStation': $scope.selectedStationYAH});
+            $scope.showNextTrainTime = departTime.query({ 'vOriginStation': $scope.selectedStationYAH });
+        };
+
+        if ($scope.showNextTrainTime[0] == "no trains to display") {
+            $scope.showNextTrainTime = 'Nothing to show now. Please try again later';
+            
+            return $scope.showNextTrainTime;
+        };
+        
+        return $scope.showNextTrainTime;
+    };
+
+    $scope.getStationSchedule = function () {
+
+        $scope.stationScheduleAll = stationSchedule.get({ 'vOriginStation': $scope.selectedStationYAH });
+
+        return $scope.stationScheduleAll;
+    };
 
 });
--- routes/yah.js
+++ routes/yah.js
@@ -8,7 +8,6 @@ var util = require('util');
 var config = require('../config_bartapi');
 
 var http = require('http');
-
 /*
 var vCmd = '';
 var vOrig = '';
@@ -17,9 +16,6 @@ var vShow = '';
 var str = '';
 var vParsed = '';
 */
-var vTemp; // [DEBUG]
-var vSave; // [DEBUG]
-
 /*
 router.route('/here')
     .get(function(req, res) {
@@ -34,19 +30,66 @@ router.route('/here')
         });
      */
 
+// Get Station Schedule information
+router.route('/stationSched')
+    .get(function (req, res) {
+        var vParsed = '';
+
+        vCmd = 'stnsched';
+        vOrig = req.query.vOriginStation;
+        vDate = 'today'; // OPTIONAL - specifies a date to be used, can use 'today' or 'now' also
+        //vI = ''; // [NOTE] unknown value here. I see it in the examples but there isn't any explanation of what it is. 
+
+        var stnsched_options = {
+            host: 'api.bart.gov',
+            path: '/api/sched.aspx?cmd=' + vCmd + '&orig=' + vOrig + '&key=' + config.bart.client
+        };
+
+        var stnsched_callback = function (response) {
+
+            response.on('data', function (chunk) {
+                vParsed += chunk;
+            });
+
+            response.on('end', function (err, result) {
+                parseString(vParsed, function (err, result) {
+                    //console.log("stnsched result = " + util.inspect(result, { showHidden: false, depth: 6 }) + "\n");
+                    
+                    console.log("stnsched result.root.uri = " + result.root.uri + "\n"); // [NOTE] url
+                    console.log("stnsched result.root.date = " + result.root.date + "\n"); // [NOTE] date as 'mm/dd/yyyy'
+                    console.log("stnsched result.root.sched_num = " + result.root.sched_num + "\n"); // [NOTE] number
+
+                    // get result.root.station.length to use as index
+                    console.log("stnsched result.root.station[0].name = " + result.root.station[0].name + "\n"); // [NOTE] long station name
+                    console.log("stnsched result.root.station[0].abbr = " + result.root.station[0].abbr + "\n"); // [NOTE] station abbreviation 'NBRK'
+
+                    console.log("stnsched result.root.station[0].item[0].$.line = " + result.root.station[0].item[0].$.line + "\n"); // [NOTE] route as 'ROUTE #'
+                    console.log("stnsched result.root.station[0].item[0].$.trainHeadStation = " + result.root.station[0].item[0].$.trainHeadStation + "\n"); // [NOTE] station abbreviation 'FRMT'
+                    console.log("stnsched result.root.station[0].item[0].$.origTime = " + result.root.station[0].item[0].$.origTime + "\n"); // [NOTE] 'h:mm AM'
+                    console.log("stnsched result.root.station[0].item[0].$.destTime = " + result.root.station[0].item[0].$.destTime + "\n"); // [NOTE] 'h:mm AM'
+                    console.log("stnsched result.root.station[0].item[0].$.trainIdx = " + result.root.station[0].item[0].$.trainIdx + "\n"); // [NOTE] number
+                    console.log("stnsched result.root.station[0].item[0].$.bikeflag = " + result.root.station[0].item[0].$.bikeflag + "\n"); // [NOTE] number
+
+                    console.log("stnsched result.root.message[0] = " + result.root.message[0] + "\n"); // [NOTE] text
+
+                    return res.send(result.root);
+                });
+            });
+        };
+        http.request(stnsched_options, stnsched_callback).end();
+    });
+
 // Real Time Departure from a given station
 router.route('/departTimeStation')
-    .get(function(req, res) {
-        var vParsed = ''; // [DEBUG]
-                
+    .get(function (req, res) {
+        var vParsed = '';
+
         vCmd = 'etd';
         vOrig = req.query.vOriginStation;
         vDir = 'n'; // [NOTE] - 'n' or 's', north or south, OPTIONAL
         vPlat = 1; // [NOTE] - 1 to 4, number of platform, OPTIONAL
-        
-        //console.log('vOriginStation' + req.query)
 
-        var xoptions = {
+        var etd_options = {
             host: 'api.bart.gov',
             path: '/api/etd.aspx?cmd=' + vCmd + '&orig=' + vOrig + '&key=' + config.bart.client
 
@@ -54,131 +97,158 @@ router.route('/departTimeStation')
             //path: '/api/etd.aspx?cmd=' + vCmd + '&orig=' + vOrig + '&key=' + config.bart.client + '&dir=' + vDir + '&plat=' + vPlat   
         };
 
-        var xcallback = function(response) {
+        var etd_callback = function (response) {
 
-            response.on('data', function(chunk) {
+            response.on('data', function (chunk) {
                 vParsed += chunk;
             });
 
-            response.on('end', function() {
-                parseString(vParsed, function(err, result) {
-                    vShow = JSON.stringify(result);
+            response.on('end', function () {
+                parseString(vParsed, function (err, result) {
 
-                    console.log("etd result.root.uri, BART API url = " + JSON.stringify(result.root.uri) + "\n");
-                    console.log("etd result.root.date, Date the call was made = " + JSON.stringify(result.root.date) + "\n");
-                    console.log("etd result.root.time, Time the call was made = " + JSON.stringify(result.root.time) + "\n");
-                    //console.log("etd result.root.station = " + util.inspect(result.root.station, { showHidden: false, depth: null }) + "\n");
+                    //console.log("etd result = " + util.inspect(result, { showHidden: false, depth: null }) + "\n"); // [DEBUG]
                     
-                    console.log("etd station array length, Station of Origin = " + result.root.station.length);
-                    var vIndexStation = result.root.station.length - 1;
                     
-                    // [NOTE] This will let me know how many trains are leaving the station at any given time
-                    console.log("etd etd array length, How many trains departing station = " + result.root.station[vIndexStation].etd.length);
-                    var vIndexEtd = result.root.station[vIndexStation].etd.length - 1;
-                    
-                    console.log("destination =" + result.root.station[vIndexStation].etd[vIndexEtd].destination);
-                    console.log("abbreviation =" + result.root.station[vIndexStation].etd[vIndexEtd].abbreviation);
+                    if (result.root.uri == null || result.root.uri == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("etd result.root.uri, BART API url = " + JSON.stringify(result.root.uri) + "\n");
+                    };
 
-                    console.log("etd estimate array length, Time for next #" + result.root.station[vIndexStation].etd[vIndexEtd].estimate.length + ' lines');                    
-                    var vIndexEstimate = result.root.station[vIndexStation].etd[vIndexEtd].estimate.length - 1;
-                    
-                    console.log("minutes until departure = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].minutes);
-                    console.log("platform trian departs = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].platform);
-                    console.log("direction of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].direction);
-                    console.log(" length of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].length);
-                    console.log("color of the trian = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].color);
-                    console.log("hexcolor of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].hexcolor);
-                    console.log("bikeflag = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].bikeflag);
-                    
-                    //console.log("etd list station number = " + result.root.station[vIndexNum].station.length);
-                    //var vStaNum = result.root.stations[vIndexNum].station.length;
+                    if (result.root.date == null || result.root.date == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("etd result.root.date, Date the call was made = " + JSON.stringify(result.root.date) + "\n");
+                    };
 
-                    vSave = JSON.stringify(result.root.station);
-                    
-                    //return JSON.stringify(result.root.station);
-                    return res.send (vSave);
+                    if (result.root.time == null || result.root.time == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("etd result.root.time, Time the call was made = " + JSON.stringify(result.root.time) + "\n");
+                    };
+
+
+                    if (result.root.station[0].etd[0].destination == null || result.root.station[0].etd[0].destination == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("etd result.root.station = " + util.inspect(result.root.station, { showHidden: false, depth: null }) + "\n");
+                        console.log("etd station array length, Station of Origin = " + result.root.station.length);
+                        var vIndexStation = result.root.station.length - 1;
+
+                        // [NOTE] This will let me know how many trains are leaving the station at any given time
+                        console.log("etd etd array length, How many trains departing station = " + result.root.station[vIndexStation].etd.length);
+                        var vIndexEtd = result.root.station[vIndexStation].etd.length - 1;
+
+                        console.log("destination station = " + result.root.station[vIndexStation].etd[vIndexEtd].destination);
+                    };
+
+                    if (result.root.station[0].etd[0].abbreviation == null || result.root.station[0].etd[0].abbreviation == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("abbreviation = " + result.root.station[vIndexStation].etd[vIndexEtd].abbreviation);
+                    };
+
+
+                    if (result.root.station[0].etd[0].estimate[0].minutes == null || result.root.station[0].etd[0].estimate[0].minutes == '') {
+                        return res.send(['no trains to display']);
+                    } else {
+                        
+                        console.log("etd estimate array length, Time for next #" + result.root.station[vIndexStation].etd[vIndexEtd].estimate.length + ' lines');
+                        var vIndexEstimate = result.root.station[vIndexStation].etd[vIndexEtd].estimate.length - 1;
+
+                        console.log("minutes until departure = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].minutes);
+
+                        console.log("platform trian departs = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].platform);
+
+                        console.log("direction of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].direction);
+
+                        console.log(" length of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].length);
+
+                        console.log("color of the trian = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].color);
+
+                        console.log("hexcolor of the train = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].hexcolor);
+
+                        console.log("bikeflag = " + result.root.station[vIndexStation].etd[vIndexEtd].estimate[vIndexEstimate].bikeflag);
+                    };
+                    return res.send(result.root.station);
                 });
             });
         };
-        
-        http.request(xoptions, xcallback).end();
-        //var vTestHttp = http.request(xoptions, xcallback).end(); // [DEBUG]
-        
-        //return res.send (vSave);
-
+        http.request(etd_options, etd_callback).end();
     });
 
 // list all BART stations
 router.route('/listAllStations')
-    .get(function(req, res) {
-        var vParsed = ''; // [DEBUG]
-        
+    .get(function (req, res) {
+        var vParsed = '';
+
         vCmd = 'stns';
 
-        var options = {
+        var stns_options = {
             host: 'api.bart.gov',
             path: '/api/stn.aspx?cmd=' + vCmd + '&key=' + config.bart.client
             //path: '/api/etd.aspx?cmd=etd&orig=RICH&key=' + config.bart.client // [DEBUG]
-                 
+
         };
 
-        var callback = function(response) {
+        var stns_callback = function (response) {
 
-            response.on('data', function(chunk) {
+            response.on('data', function (chunk) {
                 vParsed += chunk;
             });
 
-            response.on('end', function() {
-                parseString(vParsed, function(err, result) {
+            response.on('end', function () {
+                parseString(vParsed, function (err, result) {
                     vShow = JSON.stringify(result);
-                    
+
                     //if(result.root.stations == null || result.root.stations == ''){return res.send(['no trains to display'])}
 
-                    
-                    console.log('stations = ' + vShow);
-/*
-                    console.log("stns result.root.uri = " + JSON.stringify(result.root.uri) + "\n");
-                    console.log("stns result.root.stations = " + util.inspect(result.root.stations, { showHidden: false, depth: null }) + "\n");
-                    console.log("stns result.root.stations.station.name = " + JSON.stringify(result.root.stations[0].station[0].name) + "\n");
-*/
-                    
+
+                    //console.log('stations = ' + vShow);
+                    /*
+                                        console.log("stns result.root.uri = " + JSON.stringify(result.root.uri) + "\n");
+                                        console.log("stns result.root.stations = " + util.inspect(result.root.stations, { showHidden: false, depth: null }) + "\n");
+                                        console.log("stns result.root.stations.station.name = " + JSON.stringify(result.root.stations[0].station[0].name) + "\n");
+                    */
+
                     //console.log("stns result.root.stations = " + util.inspect(result.root.stations, { showHidden: false, depth: null }) + "\n");
-/*
-                    console.log("stns stations array length = " + result.root.stations.length);
-                    var vIndexNum = result.root.stations.length - 1;
-                    console.log("stns list station number = " + result.root.stations[vIndexNum].station.length);
-                    var vStaNum = result.root.stations[vIndexNum].station.length;
-
-                    // fun random numbers for data selection
-                    function randomIntFromInterval(min, max) {
-                        return Math.floor(Math.random() * (max - min + 1) + min);
-                    };
-
-                    var vRando = randomIntFromInterval(1, vStaNum);
-*/
+                    /*
+                                        console.log("stns stations array length = " + result.root.stations.length);
+                                        var vIndexNum = result.root.stations.length - 1;
+                                        console.log("stns list station number = " + result.root.stations[vIndexNum].station.length);
+                                        var vStaNum = result.root.stations[vIndexNum].station.length;
+                    
+                                        // fun random numbers for data selection
+                                        function randomIntFromInterval(min, max) {
+                                            return Math.floor(Math.random() * (max - min + 1) + min);
+                                        };
+                    
+                                        var vRando = randomIntFromInterval(1, vStaNum);
+                    */
                     //console.log(result.root.stations[vIndexNum].station[vRando].name + " station coords = \nLatitude (" + result.root.stations[vIndexNum].station[vRando].gtfs_latitude + "), \nLongitude (" + result.root.stations[vIndexNum].station[vRando].gtfs_longitude + ")\n");
 
-                    vTemp = result.root.stations[0].station;
-                    return res.send (vTemp)
-                    //vTemp = vShow; // [DEBUG]
-                    //return res.send(vTemp);
-                    //return res.send(result.root.stations[0].station);
+                    return res.send(result.root.stations[0].station)
                 });
             });
         };
 
         //var vTestHttp2 = http.request(options, callback).end(); // [DEBUG]
-        http.request(options, callback).end();
+        http.request(stns_options, stns_callback).end();
     });
 
 router.route('/callHttp')
-.get(function(req,res){
-    // load variables
-    
-    // use switch statement to call the approprate http request
-    
-    //http.request(options, callback).end();
-    
-});
+    .get(function (req, res) {
+        // load variables
+
+        // use switch statement to call the approprate http request
+
+        //http.request(options, callback).end();
+
+    });
 
 module.exports = router;
